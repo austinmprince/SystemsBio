65986e8ffdae42d7b6a2e29461d6c0b2f742c923
amprince13 <amprince@eckerd.edu>
2016-07-08 15:54:53 -0700

    Working gapFillFunction, succesfully creating major ins and outs and objective value

--- testing.py
+++ testing.py
@@ -8,14 +8,6 @@ from summaryModified import summaryModified
 printAndWriteOutput('2016_06_23_gapped_meoh_producing.xml', 'ModelSEED-reactions-db.xlsEdited - Reactions.tsv', 2
                     , False)
 
-# model = create_cobra_model_from_sbml_file('2016_06_23_gapped_meoh_producing.xml')
-# modelTest = create_cobra_model_from_sbml_file('2016_06_23_sbml.xml')
-#
-# rxn = Reaction('rxn11987')
-# model.add_reaction(rxn)
-# rxn.reaction = '3.0 cpd00001_c0 + cpd01078_c0 + 6.0 cpd11621_c0 --> 6.0 cpd00067_c0 + cpd03387_c0 + 6.0 cpd11620_c0'
-# summaryModified(model)
-
 
 
 
