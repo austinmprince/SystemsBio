4c1ced5b34e81e38b9e9843bbe4c385ab0bcc0d0
amprince13 <amprince@eckerd.edu>
2016-07-11 15:34:05 -0700

    Gapfill function updating for each iteration of rxns added

--- testing.py
+++ testing.py
@@ -1,12 +1,19 @@
-from gapFillFunction import gapFillFunc, findInsAndOuts, printAndWriteOutput
+from gapFillFunction import gapfillfunc, findInsAndOuts, printAndWriteOutput
 from cobra.io.sbml import create_cobra_model_from_sbml_file
 from cobra import flux_analysis
 from cobra import Reaction
 from summaryModified import summaryModified
 
 
-printAndWriteOutput('2016_06_23_gapped_meoh_producing.xml', 'ModelSEED-reactions-db.xlsEdited - Reactions.tsv', 2
-                    , False)
+# printAndWriteOutput('2016_06_23_gapped_meoh_producing.xml', 'ModelSEED-reactions-db.xlsEdited - Reactions.tsv', 200
+#                     , True, 'gapFillOutput.txt')
+
+model = create_cobra_model_from_sbml_file('2016_06_23_gapped_meoh_producing.xml')
+
+
+x = model.metabolites.get_by_id('cpd00047_c0')
+print(x.name)
+
 
 
 
